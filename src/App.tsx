65758eb73a6d90dca32933345add6c766241d19e
VarTy <talankina.v@mail.ru>
2024-05-14 21:38:03 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from '@mantine/core';
+import './App.scss'
+
+const App = () => {
+  return (
+    <MantineProvider>
+      <p>APP</p>
+    </MantineProvider>
+  )
+}
+
+export default App
