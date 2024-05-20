d7064b957618d1ba372ebb32b8fa0250e8bba5e5
VarTy <talankina.v@mail.ru>
2024-05-20 11:18:10 +0300

    add note input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FileInput, TextInput, MantineProvider, rem, Button, Table } from '@mantine/core';
+import { FileInput, TextInput, MantineProvider, rem, Button, Table, Input, createTheme } from '@mantine/core';
 import { IconFileInfo } from '@tabler/icons-react';
 import './App.scss';
 import { useState, useEffect } from 'react';
@@ -8,8 +8,26 @@ import useImage from 'use-image';
 interface Measurement {
   start: string,
   end: string,
-  distance: string
+  distance: string,
+  note: string
 }
+
+const theme = createTheme({
+  colors: {
+    'blue': [
+      '#ebfbee',
+      '#daf4df',
+      '#b4e7be',
+      '#8cdb9a',
+      '#6ad07b',
+      '#54c969',
+      '#48c65e',
+      '#38af4d',
+      '#2e9b43',
+      '#1f8736'
+    ],
+  }
+});
 
 const App = () => {
   const icon = <IconFileInfo style={{ width: rem(18), height: rem(18) }} stroke={1.5} />;
@@ -20,8 +38,10 @@ const App = () => {
   const [image] = useImage(!!imageURL ? imageURL : '');
   const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
   const [photoScale, setPhotoScale] = useState('20');
-  const [units, setUnits] = useState('см');
-  const [savedMeasurements, setMeasurements] = useState<Measurement[]>([])
+  const [units, setUnits] = useState('нм');
+  const [savedMeasurements, setMeasurements] = useState<Measurement[]>([]);
+
+  const scale = Math.min(window.innerWidth / 2 / imageDimensions.width, window.innerHeight / imageDimensions.height);
 
   useEffect(() => {
     if (photo) {
@@ -35,6 +55,17 @@ const App = () => {
       };
     }
   }, [photo]);
+
+  useEffect(() => {
+    if (points.length === 2) {
+      const [p1, p2] = points;
+      const dx = p2.x - p1.x;
+      const dy = p2.y - p1.y;
+      setDistance(Math.sqrt(dx * dx + dy * dy));
+    } else {
+      setDistance(null);
+    }
+  }, [points]);
 
   const handleStageClick = (e: any) => {
     const stage = e.target.getStage();
@@ -64,26 +95,60 @@ const App = () => {
         start: `${(Number(photoScale) * points[0].x / (imageDimensions.width * scale)).toFixed(1)}, ${(Number(photoScale) * points[0].y / (imageDimensions.width * scale)).toFixed(1)}`,
         end: `${(Number(photoScale) * points[1].x / (imageDimensions.width * scale)).toFixed(1)}, ${(Number(photoScale) * points[1].y / (imageDimensions.width * scale)).toFixed(1)}`,
         distance: `${(Number(photoScale) * distance / (imageDimensions.width * scale)).toFixed(2)} ${units}`,
+        note: '',
       };
       setMeasurements((state) => [...state, measurement]);
     }
   };
 
-  useEffect(() => {
-    if (points.length === 2) {
-      const [p1, p2] = points;
-      const dx = p2.x - p1.x;
-      const dy = p2.y - p1.y;
-      setDistance(Math.sqrt(dx * dx + dy * dy));
-    } else {
-      setDistance(null);
-    }
-  }, [points]);
+  const handleNoteChange = (index: number, note: string) => {
+    setMeasurements((state) => {
+      const updatedMeasurements = [...state];
+      updatedMeasurements[index].note = note;
+      return updatedMeasurements;
+    });
+  };
 
-  const scale = Math.min(window.innerWidth / 2 / imageDimensions.width, window.innerHeight / imageDimensions.height);
+  const generateCSV = (data: any) => {
+    const csvRows = [];
+    const headers = ['Начальная точка (x, y)', 'Конечная точка (x, y)', `Расстояние в ${units}`, 'Примечание'];
+    csvRows.push(headers.join(','));
+
+    data.forEach((element: any) => {
+      const row = [
+        `${element.start}`,
+        `${element.end}`,
+        `${element.distance}`,
+        `${element.note}`
+      ];
+      csvRows.push(row.join(','));
+    });
+
+    return csvRows.join('\n');
+  };
+
+  const downloadCSV = (csvContent: any, fileName = 'measurements.csv') => {
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute('href', url);
+      link.setAttribute('download', fileName);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  const handleDownload = () => {
+    const csvContent = generateCSV(savedMeasurements.reverse());
+    downloadCSV(csvContent);
+  };
 
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <div className='app'>
         {imageURL && (
           <div className='app-r-pannel'>
@@ -130,46 +195,51 @@ const App = () => {
             />
             <TextInput
               label="Единицы измерения"
-              placeholder='см'
+              placeholder='нм'
               value={units}
               onChange={handleUnitsChange}
             />
           </div>
-          <div>
-            {
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
-                <Text>
-                  Текущее измерение: {!!distance ?
-                    (Number(photoScale) * distance / (imageDimensions.width * scale)).toFixed(2) + units
-                    : "0.00" + units
-                  }
-                </Text>
-                <Button color='green.8' onClick={addMeasurement} disabled={!distance}>Добавить</Button>
-              </div>
-            }
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+              <Text>
+                Текущее измерение: {!!distance ?
+                  (Number(photoScale) * distance / (imageDimensions.width * scale)).toFixed(2) + units
+                  : "0.00" + units
+                }
+              </Text>
+              <Button color='green.8' onClick={addMeasurement} disabled={!distance}>Добавить</Button>
+            </div>
+            <Button onClick={handleDownload} color='green.8' disabled={savedMeasurements.length === 0}>Скачать CSV</Button>
           </div>
           {
             savedMeasurements.length !== 0 &&
-            <Table>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Начальная точка (x, y)</Table.Th>
-                  <Table.Th>Конечная точка (x, y)</Table.Th>
-                  <Table.Th>Расстояние</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>
-                {
-                  savedMeasurements.reverse().map((element, index) => (
-                    <Table.Tr key={index}>
-                      <Table.Td>{element.start}</Table.Td>
-                      <Table.Td>{element.end}</Table.Td>
-                      <Table.Td>{element.distance}</Table.Td>
-                    </Table.Tr>
-                  ))
-                }
-              </Table.Tbody>
-            </Table>
+            <Table.ScrollContainer minWidth={"auto"}>
+              <Table>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>Начальная точка (x, y)</Table.Th>
+                    <Table.Th>Конечная точка (x, y)</Table.Th>
+                    <Table.Th>Расстояние</Table.Th>
+                    <Table.Th>Примечание</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                  {
+                    savedMeasurements.map((element, index) => (
+                      <Table.Tr key={index}>
+                        <Table.Td>{element.start}</Table.Td>
+                        <Table.Td>{element.end}</Table.Td>
+                        <Table.Td>{element.distance}</Table.Td>
+                        <Table.Td>
+                          <Input value={element.note} onChange={(e) => handleNoteChange(index, e.target.value)} />
+                        </Table.Td>
+                      </Table.Tr>
+                    ))
+                  }
+                </Table.Tbody>
+              </Table>
+            </Table.ScrollContainer>
           }
         </div>
       </div>
